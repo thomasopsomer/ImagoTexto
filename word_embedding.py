b4001593c12c13730c01a741121f16d8075f086a
Thomas Opsomer <thomasopsomer.enpc@gmail.com>
2017-01-06 19:22:03 +0100

    in progress

--- word_embedding.py
+++ word_embedding.py
@@ -2,31 +2,60 @@
 # @Author: ThomasO
 import json
 from gensim.models import Word2Vec
+from gensim.models.phrases import Phrases
 import spacy
+import logging
+
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
 
 
 class SentenceGenerator(object):
     """ """
 
-    def __init__(self, annotations_path):
+    def __init__(self, annotations_path, mwe=False):
         """ """
-        self.data = json.loads(annotations_path)
-        self.nlp = spacy.load("en")
+        print("Load json data...")
+        with open(annotations_path) as f:
+            data = json.load(f)["annotations"]
+        print("Json data loaded")
+        # clean sentences
+        self.data = pre_clean(data, mwe)
 
     def __iter__(self):
         """ """
-        for item in self.data["annotations"]:
-            p = self.nlp(item["caption"])
-            yield [token.lemma_ for token in p if not token.is_punct]
+        for sent in self.data:
+            yield sent
+
+
+def pre_clean(data, mwe=False):
+    """ """
+    print("Cleaning data (lemmatization) ...")
+    nlp = spacy.load("en", parser=False, entity=False)
+    clean_data = []
+    for k in xrange(len(data)):
+        raw = data.pop()
+        p = nlp(raw["caption"], parse=False, entity=False)
+        clean_data.append([token.lemma_ for token in p])
+    del nlp
+    print("Data cleand.")
+    if mwe:
+        print("Learning multiwords phrases...")
+        bigram = Phrases(clean_data)
+        for k in xrange(len(clean_data)):
+            clean_data[k] = bigram[clean_data[k]]
+        print("Clean data updated with learned bigram")
+    return clean_data
 
 
 if __name__ == '__main__':
     """ """
     path = "/Users/thomasopsomer/data/mscoco/annotations/captions_train2014.json"
-    output = ""
+    output = "/Users/thomasopsomer/data/mscoco/annotations/caption_train_emb.model"
     sentences = SentenceGenerator(path)
     # train model
-    model = Word2Vec(sentences)
+    model = Word2Vec(sentences, iter=20, min_count=2, sg=1, size=128)
     # save model
     model.save(output)
 
